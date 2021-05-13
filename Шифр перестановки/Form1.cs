e4d80fe2a0c9f9edb5561b668e3f51f8b096f421
Богдан Наральник <naralnyk.b@donnu.edu.ua>
2021-05-13 17:08:03 +0300

    Добавьте файлы проекта.

---
+++ Шифр перестановки/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Шифр_перестановки
+{
+    public partial class Form1 : Form
+    {
+        shifr a;
+        public Form1()
+        {
+            InitializeComponent();
+
+            a = new shifr();
+
+            saveFileDialog1.Filter = "Text files (*.txt |*.txt |All files(*.*) | *.*";
+            openFileDialog1.Filter = "Text files (*.txt |*.txt |All files(*.*) | *.*";
+        }
+
+        private void button2_Click(object sender, EventArgs e)// Кнопка збереження файлу
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;     
+            System.IO.File.WriteAllText(filename, textBox1.Text + "\n" + textBox2.Text);
+            MessageBox.Show("Файл успішно збережений!"); ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)//Кнопка завантаження файлу
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return; 
+            string filename = openFileDialog1.FileName;
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Файл успішно відкритий!");
+        }
+        private void button1_Click(object sender, EventArgs e)// Кнопка закриття
+        {
+            Close();
+        }
+        private void button4_Click(object sender, EventArgs e)// Кнопка виконання програми
+        {
+            a.SetKey(textBox3.Text);
+            if (radioButton1.Checked)
+                textBox2.Text = a.Encrypt(textBox1.Text);
+            else
+                textBox2.Text = a.Decrypt(textBox1.Text);
+        }
+    }
+}
---
+++ Шифр перестановки/shifr.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Шифр_перестановки
+{
+    class shifr
+    {
+        private int[] key = null;
+
+        public void SetKey(int[] _key)
+        {
+            key = new int[_key.Length];
+
+            for (int i = 0; i < _key.Length; i++)
+                key[i] = _key[i];
+        }
+
+        public void SetKey(string[] _key)
+        {
+            
+            key = new int[_key.Length];
+
+            for (int i = 0; i < _key.Length; i++)
+                key[i] = Convert.ToInt32(_key[i]);
+        }
+
+        public void SetKey(string _key)
+        {
+            SetKey(_key.Split(' '));
+        }
+
+        public string Encrypt(string input)
+        {
+            for (int i = 0; i < input.Length % key.Length; i++)
+                input += input[i];
+
+            string result = "";
+
+            for (int i = 0; i < input.Length; i += key.Length)
+            {
+                char[] shifr = new char[key.Length];
+
+                for (int j = 0; j < key.Length; j++)
+                    shifr[key[j] - 1] = input[i + j];
+
+                for (int j = 0; j < key.Length; j++)
+                    result += shifr[j];
+            }
+
+            return result;
+        }
+
+        public string Decrypt(string input)
+        {
+            string result = "";
+
+            for (int i = 0; i < input.Length; i += key.Length)
+            {
+                char[] shifr = new char[key.Length];
+
+                for (int j = 0; j < key.Length; j++)
+                    shifr[j] = input[i + key[j] - 1];
+
+                for (int j = 0; j < key.Length; j++)
+                    result += shifr[j];
+            }
+
+            return result;
+        }
+    }
+}
